ace3883f46e29432ede78ca31de969c929f1d671
mcmero <7799828+mcmero@users.noreply.github.com>
2024-05-22 10:40:07 +1000

    Added primer inference to counting script
    Checks first N reads (default = 20) for a primer of
    length X (default = 30) and picks the sequence with
    the highest occurance as the primer.

--- R/count_wrapper.R
+++ R/count_wrapper.R
@@ -1,12 +1,15 @@
-count_path <- "../python/count_guides.py" # assume that cutadapt is in our path
+count_path <- "../python/count_guides.py"
 
 count <- function(fq1_trimmed,
                   guides_fasta,
                   output_dir,
                   sample_name,
-                  guide_len,
+                  guide_len = 20,
                   primer = "",
-                  mismatches = 1) {
+                  mismatches = 1,
+                  infer_primer = TRUE,
+                  num_infer = 20,
+                  primer_len = 30) {
 
   output_dir <- file.path(output_dir, paste0(sample_name, "_count"))
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
@@ -14,12 +17,20 @@ count <- function(fq1_trimmed,
   counts_out <- file.path(output_dir, paste0(sample_name, ".tsv"))
   counts_stderr <- file.path(output_dir, paste0(sample_name, "_count.out"))
 
+  primer_args <- paste0(" -p ", primer)
+  if (infer_primer) {
+    primer_args <- paste0(" -I ",
+                          " -n ", num_infer,
+                          " -P ", primer_len)
+  }
+
+  primer_arg <- ifelse(length(primer) > 0, paste0(" -p ", primer), "")
   cmd <- paste0("python ",
                 count_path,
                 " -r1 ", fq1_trimmed,
                 " -l ", guides_fasta,
                 " -i ", guide_len,
-                " -p ", primer,
+                primer_args,
                 " -m ", mismatches,
                 " > ", counts_out, " 2> ", counts_stderr)
 
